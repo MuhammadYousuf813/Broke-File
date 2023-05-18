1e91e0f8cf6aafeab9a3bee9e12c34b8c2f2364b
MuhammadYousuf813 <69145213+MuhammadYousuf813@users.noreply.github.com>
2023-05-18 11:35:56 +0500

    Create broke.py

---
+++ broke.py
@@ -0,0 +1,33 @@
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '32bit':
+
+    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls\033[1;37m")
+
+    os.system('xdg-open https://www.facebook.com/')
+
+if __name__ == "__main__":
+
+	try:		__import__("broke").menu()
+
+	except Exception as e:
+
+		exit(str(e))
+
+elif bit == '64bit':
+
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
